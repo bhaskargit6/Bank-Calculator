04cc2ffe6670cb4195a4f9cc3fb8b98f71146c31
bhaskargit6 <bhaskar.nato@gmail.com>
2026-04-21 00:18:24 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -364,9 +364,39 @@ if(!r){
     }
     let interest = maturity - (P*months);
     let tdsData = calculateTDS(interest);
+let unitText = unitVal;
+
+shareData = `
+• Monthly Deposit: ₹${formatINR(P)}
+• Rate of Interest: ${r}%
+• Duration: ${D} ${unitText}
+
+• Maturity: ${getMaturityDate(D, unitVal)}
+
+${tdsData.applicable ? `
+• Total Deposit: ₹${formatINR(P*months)}
+• Maturity (Without TDS): ₹${formatINR(maturity)}
+• Total Interest: + ₹${formatINR(interest)}
+• TDS (${tdsData.rate}%): - ₹${formatINR(tdsData.tds)}
+• Net Interest: + ₹${formatINR(tdsData.net)}
+• Maturity (With TDS): ₹${formatINR(maturity - tdsData.tds)}
+` : `
+• Total Deposit: ₹${formatINR(P*months)}
+• Maturity Amount: ₹${formatINR(maturity)}
+• Total Interest: + ₹${formatINR(interest)}
+`}
+`;
     result.innerHTML = `
+<div class="maturity-row">
+
 <div class="maturity-chip">
 <strong>Maturity</strong> :‎ ‎ ${getMaturityDate(D, unitVal)}
+</div>
+
+<div class="share-btn" onclick="exportPDF()">
+<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/></svg>
+</div>
+
 </div>
 
 <div class="result-line">
