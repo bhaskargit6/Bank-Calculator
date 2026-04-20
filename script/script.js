456d94ba17c2ffaf64d167fd2dd4e6745ba336d7
bhaskargit6 <bhaskar.nato@gmail.com>
2026-04-20 21:53:47 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,21 @@
+function shareResult(){
+
+    let text = document.getElementById("result").innerText;
+
+    if(!navigator.share){
+        alert("Sharing not supported on this device");
+        return;
+    }
+
+    navigator.share({
+        title: "Bank Calculator Result",
+        text: text
+    }).catch((err)=>{
+        console.log("Share cancelled", err);
+    });
+}
+
+
 // ==== DEFAULT =====
 
 let senior = "no";
@@ -89,8 +107,19 @@ if(!r){
     }
     let tdsData = calculateTDS(interest);
     result.innerHTML = `
+<div class="maturity-row">
+
 <div class="maturity-chip">
 <strong>Maturity</strong> :‎ ‎ ${getMaturityDate(D, unitVal)}
+</div>
+
+<div class="share-btn" onclick="shareResult()">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+<path d="M0 0h24v24H0z" fill="none"/>
+<path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"/>
+</svg>
+</div>
+
 </div>
 
 ${tdsData.applicable ? `
